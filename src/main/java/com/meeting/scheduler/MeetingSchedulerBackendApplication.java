c11e33d0fbe361c053ba695e8f398b56289f5b9d
ljw <junwoo1519@softzion.com>
2026-01-13 15:11:14 +0900

    백엔드 기본파일 푸쉬

---
+++ src/main/java/com/meeting/scheduler/MeetingSchedulerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.meeting.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSchedulerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingSchedulerBackendApplication.class, args);
+    }
+
+}
